09ebd7a7c2664e3f33d36085481c2d267299934f
Sean Newman <sean@snewman.co>
2021-08-25 21:35:37 -0400

    Fix installer script

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-scripts=$(find . -maxdepth 1 -type f -name "*.sh" | grep -v "\./_" | grep -v "install.sh")
+scripts=$(find . -maxdepth 1 -type f -name "*.sh" | sed -e 's/^\.\///' -e '/^_/d' -e '/^install.sh$/d')
 cwd=$(pwd)
+
+mkdir -p "${HOME}/bin"
 
 for script in ${scripts}; do
   if [ -f "${HOME}/bin/${script}" ]; then
     echo "${script} already exists, skipping..."
   else
+    echo "Installing ${script}"
     ln -fs "${cwd}/${script}" "${HOME}/bin/${script}"
   fi
 done
