fc3bbb73aeb93df8e11705054bf14e8931b002c1
Sean Newman <sean@snewman.co>
2021-10-25 22:38:04 -0400

    Fix number of mirrors

--- update-arch.sh
+++ update-arch.sh
@@ -3,7 +3,7 @@ set -eoux pipefail
 
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup
 temp_mirrorlist=$(mktemp)
-rate-mirrors --save "${temp_mirrorlist}" --protocol https --country-neighbors-per-country 0 --country-test-mirrors-per-country 5 arch
+rate-mirrors --save "${temp_mirrorlist}" --protocol https --country-neighbors-per-country 0 --country-test-mirrors-per-country 10 arch
 sudo mv "${temp_mirrorlist}" /etc/pacman.d/mirrorlist
 sudo pacman -Syyu
 yay --aur -Syu
