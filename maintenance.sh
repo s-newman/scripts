7e28b18287c4ecec59201583edf1af8a2dd0bc96
Sean Newman <sean@snewman.co>
2021-11-16 21:19:47 -0500

    Fix maintenance script paths

--- maintenance.sh
+++ maintenance.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-if [ -x "pacman" ]; then
+if [ -x "/usr/bin/pacman" ]; then
   mkdir -p "${HOME}/Documents/packages"
   pacman -Qqett | sort | tee "${HOME}/Documents/packages/installed-packages-$(date +'%Y-%m-%d').txt"
+else
+  echo "pacman not found, skipping package listing..."
 fi
 
-if [ -x "dconf" ]; then
+if [ -x "/usr/bin/dconf" ]; then
   mkdir -p "${HOME}/Documents/dconf-backups"
   dconf dump / | tee "${HOME}/Documents/dconf-backups/$(date +'%Y-%m-%d').txt"
+else
+  echo "dconf not found, skipping settings backup..."
 fi
