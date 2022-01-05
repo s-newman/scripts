d80a1b44f6a667c53701c7508d91fd74bbe7bc0f
Sean Newman <sean@snewman.co>
2022-01-04 19:05:59 -0500

    Swap rate-mirrors with reflector

--- update-arch.sh
+++ update-arch.sh
@@ -3,7 +3,17 @@ set -eoux pipefail
 
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup
 temp_mirrorlist=$(mktemp)
-rate-mirrors --save "${temp_mirrorlist}" --protocol https --country-neighbors-per-country 0 --country-test-mirrors-per-country 10 arch
+# Rutgers doesn't support TLS 1.2
+# Clarkson times out
+reflector \
+  --ipv4 \
+  --country US \
+  --protocol https \
+  --include '\.(edu|org)' \
+  --exclude 'rutgers' \
+  --exclude 'clarkson' \
+  --sort score \
+  --save "${temp_mirrorlist}"
 sudo mv "${temp_mirrorlist}" /etc/pacman.d/mirrorlist
 sudo pacman -Syyu
 yay --aur -Syu
