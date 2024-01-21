84a1f5277a8154c0d7752737bc2f543aeec57ab9
Sean Newman <sean@snewman.co>
2024-01-21 00:56:20 -0500

    Add "maintenance" section to update-macos script

--- update-macos.sh
+++ update-macos.sh
@@ -14,3 +14,10 @@ cargo install-update --all
 # LaTeX packages
 sudo tlmgr update --self
 sudo tlmgr update --all
+
+# Maintenance
+brew autoremove
+brew cleanup
+brew bundle dump --global --force
+cargo install-update --list > "${HOME}/.config/packages/cargo.txt"
+pipx list > "${HOME}/.config/packages/pipx.txt"
